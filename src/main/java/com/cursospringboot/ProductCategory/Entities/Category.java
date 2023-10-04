168a51594ffdd13f08081b56ba7ab277393687f7
Mickael7 <mickaelgithub7@gmail.com>
2023-10-03 21:35:35 -0300

    Product memory

--- src/main/java/com/cursospringboot/ProductCategory/Entities/Category.java
+++ src/main/java/com/cursospringboot/ProductCategory/Entities/Category.java
@@ -1,13 +1,19 @@
 package com.cursospringboot.ProductCategory.Entities;
 
-import java.io.Serializable;
-import java.util.Objects;
+import java.io.Serializable; 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private String name;
+	
+	@JsonIgnore
+	private List<Product> products = new ArrayList<>();
 
 	public Category() {
 
@@ -35,11 +41,15 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public List<Product> getProducts() {
+		return products;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result +((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -53,12 +63,12 @@ public class Category implements Serializable {
 			return false;
 		Category other = (Category) obj;
 		if (id == null) {
-		if (other.id != null)
-			return false;
+			if (other.id != null)
+				return false;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-		
+
 	}
 
 }
